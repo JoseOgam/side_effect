098f820372945ce97c85736177cd927ddc5546ec
Joseph <joseph@HP-EliteBook-820-G3-c90a9ed2.localdomain>
2023-08-21 07:12:14 +0300

    postsSlice, components and components styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getUsers } from "./redux/features/usersSlice";
 import "./App.css";
 // import ListUsers from "./components/listUsers";
